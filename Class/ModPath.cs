5cd82cbb87d2b0d6b9460a84c14d72004ca31cb4
itom <itom0717+GitHub@gmail.com>
2015-11-17 21:00:46 +0900

    作成中

---
+++ Class/ModPath.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arma3ModOptionMover
+{
+    /// <summary>
+    /// Modパス情報
+    /// </summary>
+    public class ModPath
+    {
+        /// <summary>
+        /// 絶対パス
+        /// </summary>
+        /// <returns></returns>
+        public string FullPath { get; set; } = "";
+
+        /// <summary>
+        /// パス内の*.pbo
+        /// </summary>
+        /// <returns></returns>
+        public List<string> PboFiles { get; private set; } = null;
+
+        /// <summary>
+        /// フォルダのPBOファイルを調査
+        /// </summary>
+        public void SearchPboFile()
+        {
+            //ファイル一覧取得
+            this.PboFiles = Common.File.GetFileList( this.FullPath, "*.pbo", false );
+        }
+
+        /// <summary>
+        /// PBOファイルがフォルダ内に存在するか調べる
+        /// </summary>
+        /// <param name="pboFile"></param>
+        /// <returns></returns>
+        public bool ExistsPbo( string pboFile )
+        {
+            bool isExist = false;
+
+
+            foreach ( string pbo in this.PboFiles )
+            {
+                string baseFile= Common.File.GetFileName(pbo);
+
+                if ( baseFile.Equals( pboFile, StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    //存在する
+                    isExist = true;
+                    break;
+                }
+            }
+            return isExist;
+        }
+    }
+}
---
+++ Class/ModSetting.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arma3ModOptionMover
+{
+   
+    /// <summary>
+    /// Mod設定情報
+    /// </summary>
+    public class ModSetting
+    {
+        /// <summary>
+        /// Mod情報
+        /// </summary>
+        public ModInfomation ModInfomation { get; } = new ModInfomation();
+
+        /// <summary>
+        /// MODのフルパス
+        /// </summary>
+        /// <returns></returns>
+        public string ModFullPath { get; set; } = "";
+
+        /// <summary>
+        /// addons パス情報
+        /// </summary>
+        /// <returns></returns>
+        public ModPath AddonsPathInfo { get; } = new ModPath();
+
+        /// <summary>
+        /// optionals パス情報
+        /// </summary>
+        /// <returns></returns>
+        public ModPath OptionalPathInfo { get; } = new ModPath();
+
+        /// <summary>
+        ///  Disable パス情報
+        /// </summary>
+        /// <returns></returns>
+        public ModPath DisablePathInfo { get; } = new ModPath();
+
+        /// <summary>
+        /// MOD情報取得
+        /// </summary>
+        public void GetModInfo() {
+            try {
+
+                // MODのフルパス
+                this.ModFullPath = Common.File.CombinePath( Common.File.GetApplicationDirectory() + "..",
+                                                            this.ModInfomation.ModName );
+                if ( !Common.File.ExistsDirectory( this.ModFullPath ) )
+                {
+                    //パスが見つからない
+                    throw new Exception( String.Format( Resource.TextResource.ErrMsgNotFoundModPath,
+                                                        this.ModInfomation.ModName ) );
+                }
+
+                // addonsのフルパス
+                this.AddonsPathInfo.FullPath = Common.File.CombinePath( this.ModFullPath, "addons" );
+                if ( !Common.File.ExistsDirectory( this.AddonsPathInfo.FullPath ) )
+                {
+                    //パスが見つからない
+                    throw new Exception( String.Format( Resource.TextResource.ErrMsgNotFoundModAddonsPath,
+                                                        this.ModInfomation.ModName ) );
+                }
+
+                // addonsのフルパス
+                this.OptionalPathInfo.FullPath = Common.File.CombinePath( this.ModFullPath, this.ModInfomation.OptionalsPath );
+                if ( !Common.File.ExistsDirectory( this.OptionalPathInfo.FullPath ) )
+                {
+                    //パスが見つからない
+                    throw new Exception( String.Format( Resource.TextResource.ErrMsgNotFoundModOptionalsPath,
+                                                        this.ModInfomation.ModName,
+                                                        this.ModInfomation.OptionalsPath ) );
+                }
+
+
+
+                //Disableパス
+                this.DisablePathInfo.FullPath = Common.File.CombinePath( this.ModFullPath, "_disable" );
+                if ( !Common.File.ExistsDirectory( this.DisablePathInfo.FullPath ) )
+                {
+                    //ない場合は作成しておく
+                    Common.File.CreateDirectory( this.DisablePathInfo.FullPath );
+                }
+
+
+
+                //各フォルダ内のファイルを調査しておく
+                this.AddonsPathInfo.SearchPboFile();
+                this.OptionalPathInfo.SearchPboFile();
+                this.DisablePathInfo.SearchPboFile();
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
